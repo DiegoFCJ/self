fb9801d435fba51c1595c0f5e2844f11fdabadce
diegoeffe96@gmail.com <diegoeffe96@gmail.com>
2024-11-27 11:47:07 +0100

    removing project

--- src/main/java/com/scriptagher/shared/constants/BOT_ENDPOINTS.java
+++
@@ -1,5 +0,0 @@
-package com.scriptagher.shared.constants;
-
-public class BOT_ENDPOINTS {
-    public static final String BASE_URL = "https://tuo-username.github.io/bot-list/bots/";
-}
--- start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64
-export PATH=$JAVA_HOME/bin:$PATH
-./mvnw clean install
-./mvnw javafx:run
