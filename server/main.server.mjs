ea9977b4ac20d36df134e8849c502bc5c003b6b6
DiegoFCJ <DiegoFCJ@users.noreply.github.com>
2024-12-09 12:41:12 +0000

    deploy: f5e7b608ddd4d3e6cce7f3243d1b4f0c260f9174

--- server/main.server.mjs
+++ server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-ABDBHO4J.mjs";import"./chunk-WI3ZIAVW.mjs";import"./chunk-XMLTB2OA.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-MBTQQ7RL.mjs";import"./chunk-WI3ZIAVW.mjs";import"./chunk-XMLTB2OA.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
